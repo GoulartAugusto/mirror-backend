9535e783901c57d27565850199de774de6785540
Augusto Goulart <93224568+GoulartAugusto@users.noreply.github.com>
2024-02-21 19:02:13 -0300

    old server config clean up

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const { buildSubgraphSchema } = require('@apollo/subgraph')
 const { readFileSync } = require('fs')
 const gql = require('graphql-tag')
 
-//const typeDefs = require('./schema');
 const typeDefs = gql(readFileSync('./events.graphql', { encoding: 'utf-8' }))
 const resolvers = require('./resolvers');
 const EventAPI = require('./datasources/events-api');
@@ -37,45 +36,7 @@ async function startApolloServer() {
   
   startApolloServer();
 
-  // First create this, upload at Railway and create the queries with Apollo Studio
-
-{/*
-
-The parameters to create the queries and create the mutations are these
-  {
-      id: 1,
-      title: "This is a really cool title btw",
-      description: "This is a really cool title btw and everyone likes it a lot",
-      image: "/images/BackgroundImage.png",
-      date: "01/11/2023"
-  }
-
-
-*/}
-
-{/*
-
-The old server configuration
-
-    // const { url } = await startStandaloneServer(server, {
-    //   context: async () => {
-    //     const { cache } = server;
-  
-    //     return {
-    //       dataSources: {
-    //         trackAPI: new EventAPI({ cache }),
-    //       },
-    //     };
-    //   },
-    //   listen: { port }
-    // });
-  
-    // console.log(`
-    //   🚀  Server is running
-    //   📭  Query at ${url}
-    // `);
-
-*/}
+// First create this, upload at Railway and create the queries with Apollo Studio
 
 {/*
 
