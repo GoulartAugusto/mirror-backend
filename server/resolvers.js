c1042b70b6ee193648b069367a7c70bcf4b97d16
Augusto Goulart <93224568+GoulartAugusto@users.noreply.github.com>
2024-02-29 19:54:31 -0300

    code clean

--- server/resolvers.js
+++ server/resolvers.js
@@ -12,22 +12,6 @@ const resolvers = {
         submitNewEvent: (_, { newEvent }, { dataSources }) => {
             const createEvent = dataSources.eventAPI.createNewEvent(newEvent)
             return { code: 200, success: true, message: 'success', newEvent: createEvent }
-            // try {
-            //     const event = dataSources.eventAPI.createNewEvent(id)
-            //     return {
-            //         code: 200,
-            //         success: true,
-            //         message: `Successfully created a new Event to be displayed on the homepage with id ${id}`,
-            //         event
-            //     }
-            // } catch (error) {
-            //     return {
-            //         code: error.extensions.respose.status,
-            //         success: false,
-            //         message: error.extensions.response.body,
-            //         event: null
-            //     }
-            // }
         }
     },
     Event: {
@@ -38,3 +22,5 @@ const resolvers = {
 }
 
 module.exports = resolvers;
+
+// now test the mutation
