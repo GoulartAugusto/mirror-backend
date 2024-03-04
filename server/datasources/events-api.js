ea8cbd9899bd2d114665d2533e71205eebaed3e0
Augusto Goulart <93224568+GoulartAugusto@users.noreply.github.com>
2024-03-04 19:33:22 -0300

    the queries and mutation was defined that will be utilized on the front-end

--- server/datasources/events-api.js
+++ server/datasources/events-api.js
@@ -18,3 +18,51 @@ class EventAPI {
 }
 
 module.exports = EventAPI;
+
+{/*
+
+    To implement the queries and the Mutation just use these following methods on front-end aplication
+
+    Query for All Events:
+
+    query allEvents {
+        events {
+            id
+            title
+            description
+        }
+    }
+
+    _____
+
+    Query to Get a Single Event by ID
+
+    query singleEvent($eventId: ID!) {
+        event(id: $eventId) {
+            id
+            title
+            description
+        }
+    }   
+
+    _____
+
+    Mutation to create a Event that goes to events_data.json
+
+    mutation submitNewEvent($newEvent: NewEventInput) {
+        submitNewEvent(newEvent: $newEvent) {
+            code
+            success
+            message
+            newEvent {
+            id
+            title
+            description
+            }
+        }
+    }
+
+    _____
+
+    With these code is possible to create and read data from the GraphQL server using Apollo Client in the front-end application
+*/}
